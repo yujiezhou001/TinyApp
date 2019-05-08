c462e2377adeb85c92cee9d4712df63ba748c742
Gordon <gordonzhou68@outlook.com>
2019-05-08 00:42:20 +0000

    first redirect from post request, and second from new get request.

--- express_server.js
+++ express_server.js
@@ -49,8 +49,17 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.post("/urls", (req, res) => {
     console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  });
+    const long = req.body.longURL;
+    const short = generateRandomString();
+    urlDatabase[short] = long;
+    res.redirect('urls/' + urlDatabase[short]);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+
+    const longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
